e05bd4461721bec6b02922ff6a24980d2d29e3c3
perplexedandlost <perplexedandlost@souls.xx>
2018-11-09 13:33:39 +0530

    Updates

---
+++ precache-manifest.bf24c85a0671ae2958ff19de7162c23d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/perplexed/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c61552cb1268d983f4b0",
+    "url": "/perplexed/static/js/runtime~main.c61552cb.js"
+  },
+  {
+    "revision": "fb1cf8f1786bad0aebaa",
+    "url": "/perplexed/static/js/main.fb1cf8f1.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/perplexed/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "fb1cf8f1786bad0aebaa",
+    "url": "/perplexed/static/css/main.65af03af.chunk.css"
+  },
+  {
+    "revision": "6956f987db8af41f145f874d8ddae3f9",
+    "url": "/perplexed/index.html"
+  }
+];
